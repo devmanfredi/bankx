09d7e9d0649bc499456c435fc787781acf87e11b
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-12-01 23:46:27 -0300

    Iniciado desenvolvimento.

---
+++ src/main/java/com/xcar/XcarApplication.java
@@ -0,0 +1,13 @@
+package com.xcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xcar/XcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
