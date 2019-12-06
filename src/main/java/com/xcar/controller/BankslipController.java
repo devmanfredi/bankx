9c4921901e17c71bbf2472abfe6e063035cf35f7
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-12-06 01:13:00 -0300

    Correção da url e descriçõ dos atributos do request.

--- src/main/java/com/xcar/controller/BankslipController.java
+++ src/main/java/com/xcar/controller/BankslipController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(value = "/api/v1")
+@RequestMapping("/rest")
 @Api(tags = {"Billets"}, description = "Endpoint para criação de boletos.")
 public class BankslipController {
 
@@ -27,9 +27,9 @@ public class BankslipController {
             notes = "Método utilizado para criar um Boleto"
     )
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Boleto criado", response = BankslipDTO.class),
-            @ApiResponse(code = 400, message = "Requisão mal formatada", response = ApiError.class),
-            @ApiResponse(code = 500, message = "Erro na apo", response = ApiError.class)
+            @ApiResponse(code = 201, message = "Bankslip created", response = BankslipDTO.class),
+            @ApiResponse(code = 400, message = "Bankslip not provided in the request body", response = ApiError.class),
+            @ApiResponse(code = 422, message = " Invalid bankslip provided.The possible reasons are: ○ A field of the provided bankslip was null or with invalid values", response = ApiError.class)
     })
     @PostMapping(value = "/bankslips")
     private BankslipDTO save(@RequestBody BankslipDTO bankslipDTO){
--- src/main/java/com/xcar/model/DTO/BankslipDTO.java
+++ src/main/java/com/xcar/model/DTO/BankslipDTO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -16,8 +17,18 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BankslipDTO {
+
+    @ApiModelProperty(value = "Data do boleto", example = "2019-09-16T04:27:19.034Z", required = true)
+    @NotNull
     private LocalDateTime due_date;
+
+    @ApiModelProperty(value = "Valor em centavos", example = "10000", required = true)
+    @NotNull
     private BigDecimal total_in_cents;
+
+    @ApiModelProperty(value = "Customer", example = "João da XCar", required = true)
+    @Size(min = 1, max = 120)
+    @NotNull
     private String customer;
 
     @ApiModelProperty(value = "Status do Boleto", position = 4, example = "PENDING", allowableValues = "PENDING, PAID, CANCELED", required = true)
