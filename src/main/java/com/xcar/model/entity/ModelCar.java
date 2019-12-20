18e9e18f2a8b0d83b655375160b7822a5ec7690a
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-12-20 10:56:10 -0300

    [WORK] - Create class ModelCar

---
+++ src/main/java/com/xcar/model/entity/ModelCar.java
@@ -0,0 +1,34 @@
+package com.xcar.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModelCar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 50)
+    private String name;
+
+    @NotNull
+    @ManyToOne
+    private Manufacturer manufacturer;
+
+    @NotNull
+    private LocalDate year;
+}
