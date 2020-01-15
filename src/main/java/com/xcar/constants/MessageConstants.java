07e9fb2018874d963aa52a1d55d086baf36880c2
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-01-14 23:36:30 -0300

    Constantes para caminho da api parcial(passível de ser alterado)

---
+++ src/main/java/com/xcar/constants/MessageConstants.java
@@ -0,0 +1,17 @@
+package com.xcar.constants;
+
+public interface MessageConstants {
+
+    String BANKSLIP_OK = "OK";
+
+    String BANKSLIP_CREATED = "Bankslip created";
+
+    String BANKSLIP_NOT_PROVIDED = "Bankslip not provided in the request body";
+
+    String BANKSLIP_INVALID_PROVIDED = "Invalid bankslip provided.The possible reasons are:";
+
+    String SERVICE_EXCEPTION_UNEXPECTED_ERROR = "An unexpected error occurred";
+
+    String VALIDATION_EXCEPTION_BANKSLIP_NOT_FOUND = "Bankslip not found with the specified id";
+
+}
