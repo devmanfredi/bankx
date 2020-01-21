0b25f47f667c340168d773aa221ef2dd085f0acb
heuler <felipemanfredigalaxy@gmail.com>
2020-01-20 23:42:11 -0200

    Annotations

--- src/main/java/com/xcar/model/DTO/response/BankslipStatusPay.java
+++ src/main/java/com/xcar/model/DTO/response/BankslipStatusPay.java
@@ -1,9 +1,13 @@
 package com.xcar.model.DTO.response;
 
 import com.xcar.model.enums.Status;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BankslipStatusPay {
     private Status status;
 }
