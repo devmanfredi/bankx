7f2caf193da6d663fe634eb671ceceb29901afe7
heuler <felipemanfredigalaxy@gmail.com>
2020-01-26 23:18:55 -0300

    Teste para salvar salvar boleto

--- src/test/java/com/xcar/controllers/BankslipControllerTest.java
+++ src/test/java/com/xcar/controllers/BankslipControllerTest.java
@@ -1,17 +1,31 @@
 package com.xcar.controllers;
 
+import com.xcar.builders.BankslipBuilder;
 import com.xcar.mapper.BankslipMapper;
+import com.xcar.model.DTO.BankslipDTO;
+import com.xcar.model.entity.Bankslip;
 import com.xcar.repository.BankslipRepository;
 import com.xcar.service.BankslipService;
+import org.hamcrest.Matchers;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import javax.transaction.Transactional;
+import java.util.UUID;
+
+import static com.xcar.util.TestUtil.convertObjectToJsonBytes;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -33,5 +47,18 @@ public class BankslipControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Test
+    public void dadoBoleto_quandoSalvar_entaoDeveRetornarBoleto() throws Exception {
+        Bankslip bankslip = BankslipBuilder.bankslip(UUID.randomUUID().toString()).build();
+        BankslipDTO bankslipDTO = bkMapper.toDTO(bankslip);
+        Mockito.when(bkRepository.save(bankslip)).thenReturn(bankslip);
+
+        ResultActions boletoSalvo = mvc.perform(post("/rest/bankslips")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(convertObjectToJsonBytes(bankslipDTO)))
+                .andExpect(status().isOk());
+        boletoSalvo.andExpect(jsonPath("$.status", Matchers.is(bankslip.getStatus().toString())));
+        boletoSalvo.andExpect(jsonPath("$.customer", Matchers.is(bankslip.getCustomer())));
+    }
 
 }
