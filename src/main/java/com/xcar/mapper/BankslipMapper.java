de428226ac23ff8c916996bafe5ecb7158e0f2ad
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-04-10 18:20:43 -0300

    Alteração da mensagem de retorno.

--- src/main/java/com/xcar/mapper/BankslipMapper.java
+++ src/main/java/com/xcar/mapper/BankslipMapper.java
@@ -63,9 +63,8 @@ public interface BankslipMapper {
     BankslipStatusPay toStatusBeforePut(Bankslip bankslip);
 
     @Mappings({
-            @Mapping(target = "status", expression = "java(\"204\")"),
-            @Mapping(target = "message", expression = "java(\"OK\")"),
-            @Mapping(target = "bankslip.id", source = "id"),
+            @Mapping(target = "status", expression = "java(\"200\")"),
+            @Mapping(target = "message", expression = "java(\"Bankslip paid\")"),
             @Mapping(target = "bankslip.status", source = "status")
     })
     ResponsePayDTO toDTOPay(Bankslip bankslip);
